81961e3b7d2678ffd8ada90955b59f1094c21458
Jon Rafkind <jon@rafkind.com>
2023-05-08 00:16:35 -0700

    change buttons, mess around with animations

--- js/code.js
+++ js/code.js
@@ -50,8 +50,44 @@ function addScore(playerAdd, playerSame){
 }
 
 function player1AddScore(){
+    let plotDiv = document.getElementById('plot');
+
+    // Plotly.extendTraces('plot', {x: [[player1.x[player1.x.length - 1] + 1]], y: [[player1.y[player1.y.length - 1] + 1]]}, [0]);
+    // Plotly.extendTraces('plot', {x: [[player2.x[player2.x.length - 1] + 1]], y: [[player2.y[player2.y.length - 1]]]}, [1]);
+    // Plotly.redraw('plot');
     addScore(player1, player2);
     Plotly.redraw('plot');
+    /*
+    var p1 = {x: [...player1.x] + [player1.x[player1.x.length-1]], y: [...player1.y] + [player1.y[player1.y.length-1]]}
+    p1 = {x: [...player1.x].concat([player1.x[player1.x.length-1]+1]), y: [...player1.y].concat([player1.y[player1.y.length-1]])}
+    Plotly.animate('plot', {data: [p1], traces: [0], layout: {}},
+        {
+        transition: {
+            duration: 500,
+            easing: 'cubic-in-out'
+        },
+        frame: {
+            duration: 500
+        }
+    })
+    */
+
+    /*
+    let transition = {
+            transition: {
+                duration: 500,
+                easing: 'cubic-in-out'
+            },
+            frame: {
+                duration: 500
+            }
+    }
+
+    Plotly.animate('plot', {data: [{x: [0, 1, 2, 3], y: [0, 1, 3, 5, 7]}], traces: [0], layout: {}}, transition).then(function(){
+    }) 
+    Plotly.redraw('plot');
+    */
+    
 } 
 
 function player2AddScore(){
